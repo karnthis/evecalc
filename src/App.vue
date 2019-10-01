ac3c8dbab4d085400b0c506749bf76d64e9f6a93
Erin RIvas <erin@atomicbear.com>
2019-09-30 19:04:26 -0500

    working baseline

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <v-container>
+        <v-row>
+          <v-textarea rows="6" id="calcField">
+
+          </v-textarea>
+        </v-row>
+        <v-row>
+          <v-btn @click="process">
+            press me
+          </v-btn>
+        </v-row>
+        <v-row>
+          {{ returned }}
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+  },
+  data: () => ({
+    returned: ''
+  }),
+  methods: {
+    process() {
+      fetch("http://localhost:5115/", {
+    method: "POST",
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'text/plain',
+      // 'User-Agent': `TcGre'l en Karnt`
+    },
+    body: document.querySelector("#calcField").value
+  })
+  .then(res => res.json())
+  // .then(res => res.text())
+  .then(res => {
+    this.returned = res
+  })
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
