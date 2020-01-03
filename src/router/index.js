2b7746bb5ec7150a0488a05536d70eacb0103f67
Erin RIvas <erin@atomicbear.com>
2020-01-02 20:20:41 -0600

    basic rework

--- src/router/index.js
+++ src/router/index.js
@@ -11,24 +11,6 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/share-calc',
-    name: 'share-calc',
-    component: () => import(/* webpackChunkName: "share-calc" */ '../views/ShareCalc.vue')
-  },
-  {
-    path: '/contracts',
-    name: 'contracts',
-    component: () => import(/* webpackChunkName: "contracts" */ '../views/Contracts.vue')
-  },
-  {
     path: '/buying',
     name: 'buying',
     component: () => import(/* webpackChunkName: "buying" */ '../views/Buying.vue')
@@ -39,9 +21,14 @@ const routes = [
     component: () => import(/* webpackChunkName: "ore-calc" */ '../views/OreCalc.vue')
   },
   {
-    path: '/shipping',
-    name: 'shipping',
-    component: () => import(/* webpackChunkName: "shipping" */ '../views/Shipping.vue')
+    path: '/share-calc',
+    name: 'share-calc',
+    component: () => import(/* webpackChunkName: "share-calc" */ '../views/ShareCalc.vue')
+  },
+  {
+    path: '/subway',
+    name: 'subway',
+    component: () => import(/* webpackChunkName: "subway" */ '../views/Subway.vue')
   },
 ]
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,67 @@
 <template>
   <div class="home text-center">
-    <Soon pagename="Homepage"/>
+    <!-- <Soon pagename="Homepage"/> -->
+    <!-- <h2>Welcome</h2>
+    <p>Ø We're based in Hi-Sec and have no minimum skill point requirement.</p>
+    <p>Ø We Mainly Do Invasions and WH day-trips for ISK.</p>
+    <p>Ø Lowsec and Wormhole PvP</p>
+    <p>Ø NPSI fleets are also encouraged</p>
+    <p>Ø Small corporation: This means you get to know everyone and are not just a number.</p>
+    <p>
+      Ø
+      <a href="https://evewho.com/corporation/98530505" target="_blank">Check our EVEWho</a>
+    </p>
+    <p>Ø No Politics</p>
+    <p>Ø Real Life > Space Ships in our group.</p>
+    <p>Ø We don't care about Zkill, so do what you will with your toys.</p>
+    <p>Ø Interested? Join our discord server and say hi or ask questions!</p>
+    <p>Ø EveSeat As Well when you Apply!</p>
+    <p>Ø Wouldn't you like to be a Pepper too?</p>-->
+
+    <v-container fluid fill-height>
+      <v-layout align-center justify-center>
+        <!-- <v-row> -->
+          <v-card class="justify-center">
+            <v-img contain height="128px" src="../assets/sh-logo.png"></v-img>
+            <v-card-title class="justify-center">
+              <span>Welcome</span>
+            </v-card-title>
+            <v-card-text class="text-left">
+              <p>Ø We're based in Hi-Sec and have no minimum skill point requirement.</p>
+              <p>Ø We Mainly Do Invasions and WH day-trips for ISK.</p>
+              <p>Ø Lowsec and Wormhole PvP</p>
+              <p>Ø NPSI fleets are also encouraged</p>
+              <p>Ø Small corporation: This means you get to know everyone and are not just a number.</p>
+              <p>
+                Ø
+                <a href="https://evewho.com/corporation/98530505" target="_blank">Check our EveWho</a>
+              </p>
+              <p>Ø No Politics</p>
+              <p>Ø Real Life > Space Ships in our group.</p>
+              <p>Ø We don't care about Zkill, so do what you will with your toys.</p>
+              <p>Ø Interested? Join our discord server and say hi or ask questions!</p>
+              <p>Ø EveSeat As Well when you Apply!</p>
+              <p>Ø Wouldn't you like to be a Pepper too?</p>
+            </v-card-text>
+          </v-card>
+        <!-- </v-row> -->
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Soon from '@/components/Soon.vue'
+// import Soon from '@/components/Soon.vue'
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    Soon
+    // Soon
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
